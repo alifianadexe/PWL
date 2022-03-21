e7b594c0c6bfaacf0f37562f9c85779a39dc02ac
alifianadexe <alifianadexe@gmail.com>
2022-03-21 21:43:28 +0700

    adding week 3

---
+++ minggu3/praktikum/inputcheckbox.php
@@ -0,0 +1,20 @@
+<html>
+
+<head>
+    <title>Band Favorit ~ Inputan Checkbox</title>
+</head>
+
+<body>
+    <FORM ACTION="prosescheckbox.php" METHOD="POST" NAME="input">
+        <h2>Pilih Band Favorit Anda :</h2>
+        <input type="checkbox" name="band01" value="YOASOBI" checked>
+        YOASOBI<br>
+        <input type="checkbox" name="band02" value="King GNU">
+        King GNU<br>
+        <input type="checkbox" name="band03" value="ONE OK ROCK">ONE OK ROCK<br>
+        <input type="checkbox" name="band04" value="Mandragan">Mandragan<br>
+        <input type="submit" name="Pilih" value="Pilih">
+    </FORM>
+</body>
+
+</html>
